4d5714db28921a42efe80c130650f509921d3618
Alya Carina <alyacarina@gmail.com>
2017-05-16 20:50:43 -0400

    Initial commit

---
+++ Source/SmallWorld/LionelI.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SmallWorld.h"
+#include "LionelI.h"
+
+
+// Sets default values
+ALionelI::ALionelI()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ALionelI::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALionelI::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/SmallWorld/SmallWorldGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "SmallWorldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SMALLWORLD_API ASmallWorldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
